757128d2ab2c23d6c31399973251b689a4be4e69
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-06-02 13:43:33 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <link href="style.css" media="all" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;700&display=swap" rel="stylesheet" />
     </head>
 
       <main className={styles.main}>
