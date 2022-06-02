0a5ab02aff89694ea394767c24ca888889490861
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-06-02 14:13:46 +0900

    add footer

--- pages/index.js
+++ pages/index.js
@@ -55,62 +55,48 @@ export default function Home() {
             </a></section>
         </div>
 
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-             Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h1 className="contents">Profile</h1>
+        <div>
+            <img className="prof" src="img/prof.jpg" alt="プロフィール画像" />
+            <div className="info">
+                <table>
+                    <tr>
+                        <td className="prof_l">名前</td>
+                        <td className="prof_r">つよぽん</td>
+                    </tr>
+                    <tr>
+                        <td className="prof_l">性別</td>
+                        <td className="prof_r">男性</td>
+                    </tr>
+                    <tr>
+                        <td className="prof_l">年齢</td>
+                        <td className="prof_r">おおよそ20歳</td>
+                    </tr>
+                    <tr>
+                        <td className="prof_l">所属</td>
+                        <td className="prof_r">静岡大学 情報学部 情報科学科</td>
+                    </tr>
+                    <tr>
+                        <td className="prof_l">居住</td>
+                        <td className="prof_r">静岡県</td>
+                    </tr>
+                    <tr>
+                        <td className="prof_l">出身</td>
+                        <td className="prof_r">奈良県</td>
+                    </tr>
+                </table><br />
+                <a href="https://twitter.com/Tsuyopon_1067" target="_blank" rel="noopener noreferrer"><img src="img/prof_tw.png" />Twitter(@Tsuyopon_1067)</a><br />
+                <a href="https://github.com/" target="_blank" rel="noopener noreferrer"><img src="img/prof_github.png" />GitHub</a>
+            </div>
         </div>
 
-        <h1 classMain="contents">Profile</h1>
-        <iframe classMain="prof" src="./prof.html"></iframe>
-
-        <iframe classMain="foot" src="./foot.html"></iframe>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <footer>
+        (c) 2022 つよぽん all rights reserved.<br />
+        オリジナルのThe Go gopher（Gopherくん）は、Renée Frenchによってデザインされました。
+        </footer>
       </footer>
     </div>
   )
